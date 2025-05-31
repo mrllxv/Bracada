58e5ca2e25db1a346a7d713b88e43842b22d69c2
Victtor Marcelo <victtordesousa1@gmail.com>
2025-05-30 23:33:20 -0300

    add php pages

---
+++ pages/athletes/athletes.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+
+    <link rel="stylesheet" href="../../styles/global.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Oxygen:wght@300;400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+    <title>Home | Braçada final</title>
+</head>
+
+<body class="body">
+    <!-- Para nomeção de classes aqui no html, estamos utlizando a convenção  de nomenclatura BEM css para facilitar na organização do codigo e na especificidade do CSS -->
+
+    <!-- BEM é um acrônimo para Block, Element e Modifier onde
+      Block é uma entidade autônoma que é significativa por si só. 
+      Exemplo -> header, container, menu, checkbox,input
+
+      Element é uma parte de um bloco que não tem significado independente e está semanticamente vinculada ao seu bloco, como se fosse um elemento-filho
+      Exemplo -> menu item, list item, checkbox caption, header title
+
+      Modifier é uma flag em um bloco ou elemento que são usadas para referneciar alterações do comportamento da entidade.
+      Exemplo -> disabled, highlighted, checked, fixed, size big,color yellow
+    -->
+
+    <!-- Syntax
+      uma classe CSS composta de Nome do Bloco + 2 underlines + nome do Elemento: .[Bloco]__[Elemento].
+      Exemplo -> header__home-btn (onde o header é o bloco e o elemento é o home-btn)
+
+      caso o block ou elemento tiver um modifier, a escrita da classe é composta pelo 
+      nome de um Bloco ou Modificador + 2 hífens + o nome do Modificador: [Bloco|Elemento]--[Modificador].
+      Exemplo -> nav__buttons--hover (O bloco é nav, buttons é o elemento e o modificador de buttons é o hover)
+
+
+      Para mais informações e sanar as dúvidas, consultar as documentações abaixo:
+      https://desenvolvimentoparaweb.com/css/bem/
+      https://getbem.com/introduction/
+    -->
+
+    <!-- Estruturando o cabeçalho do site e a barra de navegação -->
+    <header class="header header-athletes">
+        <nav class="header__nav">
+            <a href="../homepage/index.html" class="nav__home-btn">
+                <h1 class="nav__title">Braçada Final</h1>
+            </a>
+            <ul class="nav__list">
+                <li class="nav__list-item nav__list-item--hover nav__list-item--focus">
+                    <a href="../homepage/index.html" class="nav__list-link">Home</a>
+                </li>
+                <li class="nav__list-item nav__list-item--hover nav__list-item--focus">
+                    <a href="../pages/atletas/athletes.html" class="nav__list-link">Atletas</a>
+                </li>
+                <li class="nav__list-item nav__list-item--hover nav__list-item--focus">
+                    <a href="../pages/medals/medalhas.html" class="nav__list-link">Medalhas</a>
+                </li>
+                <li class="nav__list-item nav__list-item--hover nav__list-item--focus">
+                    <a href="../gallery/gallery.html" class="nav__list-link">Galeria</a>
+                </li>
+                <li class="nav__list-item nav__list-item--hover nav__list-item--focus">
+                    <a href="#" class="nav__list-link">Quiz</a>
+                </li>
+            </ul>
+
+            <div class="nav__container-buttons">
+                <a href="../register & login/register.html"
+                    class="button nav__buttons nav__buttons--hover nav__buttons--focus">
+                    Cadastrar
+                </a>
+                <a href="../register & login/login.html"
+                    class="button nav__buttons nav__buttons--hover nav__buttons--focus">
+                    Entrar
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <section class="athletes">
+        <div class="athletes__search-container">
+            <h2 class="athletes__title">Catálogo de atletas</h2>
+            <input type="search" name="search_athlete" id="search_athlete" class="input athletes__input-search">
+
+            <select name="" id="">
+                <option value="none">Nome do</option>
+                <option value="country">País</option>
+            </select>
+
+            <select name="" id="">
+                <option value="none">Nome do</option>
+                <option value="modality">modalidade</option>
+            </select>
+
+        </div>
+    </section>
+
+    <main class="main">
+
+    </main>
+
+    <div class="arrow-up hidden">
+        <img src="../../icons/arrow.svg" alt="">
+    </div>
+
+    <footer id="footer">
+        <p class="footer__infos">
+            2025 - Projeto Braçada Final -
+            <a href="https://github.com/mrllxv/BracadaFinal" target="_blank" class="footer__infos-github">Github</a>
+        <p class="footer__infos-advice">Este projeto é apenas para cunho educativo e totalmente baseado no site oficial
+            dos
+            <a href="https://www.olympics.com/pt/" class="footer__infos-reference">Jogos Olímpicos</a>.
+            Todos os direitos e informações são reservados a eles
+        </p>
+        </p>
+    </footer>
+
+</body>
+
+</html>
