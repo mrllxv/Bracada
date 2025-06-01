7e1b095ebfede88393dce648c65470c2f36e3bd5
Victtor Marcelo <victtordesousa1@gmail.com>
2025-06-01 01:08:56 -0300

    fix some endpoints

--- views/cadastroForm.php
+++ views/cadastroForm.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esqueci a Senha</title>
+    <title>Cadastrar</title>
     <link rel="stylesheet" href="../styles/global.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
