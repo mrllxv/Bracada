fcedea972b98d708e655e59bde85558dd36b273f
Murillo Aragão <murilloaragaao@gmail.com>
2025-06-01 03:39:30 -0300

    fix some things

--- pages/athletes/scripts/getAthleteProfile.js
+++ pages/athletes/scripts/getAthleteProfile.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const params = new URLSearchParams(window.location.search);
-
-  
   const atletaId = parseInt(params.get('id'), 10); 
 
   if (isNaN(atletaId)) {
@@ -9,31 +7,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     return;
   }
 
-
   try {
-    const response = await fetch(`../../controller/atletaController.php?id=${atletaId}`);
+    const response = await fetch(`../../controllers/atletaController.php?id=${atletaId}`);
     const atleta = await response.json();
-  fetch(`../../controllers/atletaController.php?id=${atletaId}`)
-    .then(res => res.json())
-    .then(atleta => {
-    
 
     if (atleta.erro) {
       document.body.innerHTML = `<p>${atleta.erro}</p>`;
       return;
     }
 
-    document.querySelector(".athlete-presentation__name").textContent =
-      atleta.nome;
-    document.querySelector(".athlete-presentation__infos-country").textContent =
-      atleta.pais;
-    document.querySelector(
-      ".athlete-presentation__infos-modality"
-    ).textContent = atleta.modalidade;
-    document.querySelector(".athlete-biography__content").textContent =
-      atleta.biografia;
+    document.querySelector(".athlete-presentation__name").textContent = atleta.nome;
+    document.querySelector(".athlete-presentation__infos-country").textContent = atleta.pais;
+    document.querySelector(".athlete-presentation__infos-modality").textContent = atleta.modalidade;
+    document.querySelector(".athlete-biography__content").textContent = atleta.biografia;
+
   } catch (error) {
     console.error("Erro ao carregar atleta:", error);
     document.body.innerHTML = "<p>Erro ao carregar os dados do atleta.</p>";
   }
 });
+
