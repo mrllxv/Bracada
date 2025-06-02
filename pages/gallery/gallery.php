5fe03546c2d7d56bc55e555e761e48bdb4a808c0
Victtor Marcelo <victtordesousa1@gmail.com>
2025-06-02 00:33:06 -0300

    finishing gallery page

--- pages/gallery/gallery.php
+++ pages/gallery/gallery.php
@@ -22,7 +22,7 @@ require_once '../../actions/verifica_login.php';
     <title>Home | Braçada final</title>
 </head>
 
-<body class="body">
+<body class="body body-gallery">
     <!-- Para nomeção de classes aqui no html, estamos utlizando a convenção  de nomenclatura BEM css para facilitar na organização do codigo e na especificidade do CSS -->
 
     <!-- BEM é um acrônimo para Block, Element e Modifier onde
@@ -84,6 +84,16 @@ require_once '../../actions/verifica_login.php';
         </nav>
     </header>
 
+    <section class="intro-gallery">
+        <div class="intro-gallery__container">
+            <h2 class="intro-gallery__title">Reviva Grandes Momentos Olímpicos</h2>
+            <p class="intro-gallery__text">
+                A cada braçada, salto ou chegada, um instante é eternizado. Nesta galeria, você encontrará registros visuais que celebram a emoção, a superação e a glória dos esportes olímpicos. De atletas consagrados a momentos inesquecíveis, mergulhe nas imagens que contam as histórias que moldaram o espírito olímpico.
+            </p>
+        </div>
+    </section>
+    
+
     <main class="main">
         <!-- Modal de imagem -->
         <div class="modal hidden" id="imageModal">
@@ -97,22 +107,111 @@ require_once '../../actions/verifica_login.php';
         </div>
 
         <section class="banner-gallery">
-            <div class="banner-gallery__container">
-                <h2 class="banner-gallery__title">Galeria de imagens</h2>
-                <p class="banner-gallery__description">Confira os momentos mais marcantes dos esportes olímpicos</p>
-            </div>
             <div class="gallery__container">
                 <article class="gallery__card">
                     <div class="gallery__card-thumbnail">
-                        <button class="gallery__card-button--expand">
+                        <img src="../gallery/galleryAssets/Australia/Ariarne Titmus.jpg" alt="Ariarne Titmus" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/Australia/Ariarne Titmus.jpg">
                             <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
                         </button>
-                        <img src="../../assets/Olympic-medals.jpg" alt="" class="gallery__card-image">
                     </div>
-                    <p class="gallery"></p>
-
+                    <p class="gallery__card-description">Titmus conquistou ouro nos 200 m e 400 m livre em Tóquio 2020, prata nos 800 m e bronze no 4×100 m livre. Em 2023, venceu os 400 m no Mundial de Fukuoka com recorde mundial, foi prata nos 200 m, bronze nos 800 m e ouro no 4×200 m com novo recorde. Em Paris 2024, voltou a vencer os 400 m livre, reafirmando seu domínio.</p>
                 </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="galleryAssets/Australia/Kaylee McKeown.webp" alt="Kaylee McKeown" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="galleryAssets/Australia/Kaylee McKeown.webp"">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">McKeown brilhou em Tóquio 2020 com 3 ouros e 1 bronze. Em 2022, somou 2 ouros, 3 pratas e 1 bronze em Budapeste e Melbourne. Em 2023, venceu os 50, 100 e 200 m costas no Mundial de Fukuoka. Em Paris 2024, foi ouro nos 100 m costas com recorde olímpico.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/Brasil/ana-marcela-cunha-683d01c09d281.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/Brasil/ana-marcela-cunha-683d01c09d281.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Ana Marcela Cunha, nadadora brasileira e campeã olímpica em Tóquio 2020, é uma das maiores maratonistas aquáticas da história. Venceu seis vezes o prêmio de melhor do mundo e tem 14 medalhas em mundiais, sendo 7 ouros. Também é referência LGBTQIA+ no esporte.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/Brasil/fernando-scheffer-683d01c0cca88.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/Brasil/fernando-scheffer-683d01c0cca88.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Fernando Scheffer é um nadador brasileiro, bronze nos 200 m livres em Tóquio 2020. Destaque desde 2018, quebrou vários recordes sul-americanos e foi ouro no Mundial de 2018 no 4×200 m livre com recorde mundial. Brilhou também nos Jogos Pan-Americanos.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/Canada/summer-mcintosh-683d01cc01fd9.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/Canada/summer-mcintosh-683d01cc01fd9.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+
+                    <p class="gallery__card-description">Summer McIntosh, nadadora canadense, é tricampeã olímpica e tetracampeã mundial. Aos 14 anos foi destaque em Tóquio 2020. Quebrou dois recordes mundiais em 2023 e é campeã em estilos livre, borboleta e medley, sendo considerada fenômeno da natação.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/Canada/penny-oleksiak-683d01cbd9027.jpg" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/Canada/penny-oleksiak-683d01cbd9027.jpg">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Penny Oleksiak, nadadora canadense, começou aos 9 anos. Nos Jogos Rio 2016, foi a primeira canadense a conquistar 4 medalhas: ouro nos 100m livre, prata nos 100m borboleta e bronze nos revezamentos 4x100m e 4x200m livre. É uma das maiores estrelas da natação mundial..</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/China/qin-haiyang-683d01d20de8c.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/China/qin-haiyang-683d01d20de8c.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Qin Haiyang e um nadador chines especializado em peito e medley. Nos Jogos Tokio 2020 foi desclassificado nos 200m peito. No Mundial de 2023 em Fukouka, ganhou tres ouros nos 50m, 100m peito e revezamento 4x100m medley misto, e detem o recorde asiatico dos 50m peito.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/China/zhang-yufei-683d01d361c6f.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/China/zhang-yufei-683d01d361c6f.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Zhang Yufei e nadadora chinesa especialista em livre e borboleta. Detem recorde mundial junior nos 200m borboleta e tem 7 medalhas em eventos internacionais. Em Rio 2016 competiu e em Tóquio 2020 ganhou ouro nos 200m borboleta com recorde olimpico.</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/EUA/Caeleb Dressel.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/EUA/Caeleb Dressel.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Dressel competiu nos Jogos Olímpicos de 2016, conquistando a medalha de ouro nos 4x100 m medley e 4x100 m livre. Ganhou cinco ouros em Tóquio 2020, os quais foram obtidos nas provas quatro por cem metros livre, quatro por cem metros medley, cem metros livre, cem metros borboleta e cinquenta metros livre..</p>
+                </article>
+
+                <article class="gallery__card">
+                    <div class="gallery__card-thumbnail">
+                        <img src="../gallery/galleryAssets/EUA/Micheal_Phelps.webp" alt="Medalhas olímpicas" class="gallery__card-image">
+                        <button class="gallery__card-button--expand" data-image="../gallery/galleryAssets/EUA/Micheal_Phelps.webp">
+                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                        </button>
+                    </div>
+                    <p class="gallery__card-description">Michael Phelps e ex-nadador dos EUA, maior medalhista olimpico com 23 ouros. Em Pequim 2008, ganhou 8 ouros em uma edicao. Em Londres 2012, bateu recorde de medalhas totais com 19. Voltou em 2016 e quebrou recordes de idade e ouros em revezamentos.</p>
+                </article>
+            
+                
             </div>
+            
         </section>
     </main>
 
@@ -120,18 +219,19 @@ require_once '../../actions/verifica_login.php';
         <img src="../../icons/arrow.svg" alt="">
     </div>
 
-    <footer id="footer">
+    <footer class="footer" id="footer">
         <p class="footer__infos">
             2025 - Projeto Braçada Final -
-            <a href="https://github.com/mrllxv/BracadaFinal" target="_blank" class="footer__infos-github">Github</a>
+            <a href="https://github.com/mrllxv/Bracada" target="_blank" class="footer__infos-github footer__link">Github</a>
         <p class="footer__infos-advice">Este projeto é apenas para cunho educativo e totalmente baseado no site oficial
             dos
-            <a href="https://www.olympics.com/pt/" class="footer__infos-reference">Jogos Olímpicos</a>.
+            <a href="https://www.olympics.com/pt/" target="_blank" class="footer__infos-reference footer__link">Jogos Olímpicos</a>.
             Todos os direitos e informações são reservados a eles
         </p>
         </p>
     </footer>
 
+    <script src="./scripts/gallery.js"></script>
 </body>
 
 </html>
